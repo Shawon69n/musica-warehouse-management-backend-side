09d05efde00aea113814a8ea13306ed58cb3d242
ShawonsGit <s.shawon0000@gmail.com>
2022-05-08 19:36:42 +0600

    mongo connected and data loaded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,39 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+//username: musicadb
+//password : iM41Rl8LLMM5QBrb
+
+
+const uri = `mongodb+srv://musicadb:iM41Rl8LLMM5QBrb@assignment-11.zbngn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+        await client.connect();
+    const serviceCollection = client.db('Musica').collection('products');
+
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
 
 
 app.get('/' , (req,res)=>{
